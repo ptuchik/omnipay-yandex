179baf31027b8c5b5aa633582400d26b1e28bd47
Samvel Sargsyan <samvel.sargsyan@betconstruct.com>
2018-08-31 20:26:43 +0400

    fix

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -2,9 +2,7 @@
 
 namespace Omnipay\Yandex\Message;
 
-use Omnipay\Common\Message\AbstractResponse;
-use Omnipay\Yandex\Helpers\ParametersTrait;
-use YandexCheckout\Model\PaymentStatus;
+
 
 /**
  * Class CompletePurchaseResponse
