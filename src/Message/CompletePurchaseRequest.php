999d08231e46091a140432f3c694c7a8f6618ec6
Samvel Sargsyan <samvel.sargsyan@betconstruct.com>
2018-08-31 20:19:33 +0400

    add complete purchase request clases

---
+++ src/Message/CompletePurchaseRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Omnipay\Yandex\Message;
+
+
+/**
+ * Class CompletePurchaseRequest
+ * @package Omnipay\Yandex\Message
+ */
+class CompletePurchaseRequest extends PurchaseRequest
+{
+
+    /**
+     * Send data and return response instance
+     *
+     * @param mixed $data
+     *
+     * @return \Omnipay\Common\Message\ResponseInterface|\Omnipay\Idram\Message\PurchaseResponse
+     */
+    public function sendData($data)
+    {
+        
+        $response =  $this->yandex->capturePayment(['amount' => array_get($data, 'amount', [])], array_get($data, 'payment_method_id'));
+
+        return $this->response = new PurchaseCompleteResponse($this, $response);
+    }
+}
---
+++ src/Message/CompletePurchaseResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Omnipay\Yandex\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Yandex\Helpers\ParametersTrait;
+use YandexCheckout\Model\PaymentStatus;
+
+/**
+ * Class CompletePurchaseResponse
+ * @package Omnipay\Yandex\Message
+ */
+class CompletePurchaseResponse extends PurchaseResponse
+{
+    
+
+}
